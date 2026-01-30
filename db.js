6d35e668cf1f4dc4ac7cdc50af4f093ea7ef5edf
lanjib13 <lanjbcawaru1967@gmail.com>
2026-01-30 17:47:49 +0700

    aaa

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 module.exports = db;
